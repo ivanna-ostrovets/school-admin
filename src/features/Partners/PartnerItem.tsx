b5ddd4d00200ce4620ca455f0e096cc0970e5572
Ivanna Ostrovets <ivanna.ostrovets@gmail.com>
2022-02-14 00:47:16 +0200

    Add confirmation dialog on partner removal

--- src/features/Partners/PartnerItem.tsx
+++ src/features/Partners/PartnerItem.tsx
@@ -11,6 +11,7 @@ import ListItemText from '@mui/material/ListItemText';
 import TextField from '@mui/material/TextField';
 import React, { KeyboardEvent, useState } from 'react';
 import { removePartner, updatePartner } from '../../api/partnersApi';
+import ConfirmDeleteDialog from '../../components/ConfirmDeleteDialog';
 import { Partner, Partners } from '../../types';
 
 interface Props {
@@ -20,6 +21,7 @@ interface Props {
 
 function PartnerItem({ partner, setPartners }: Props) {
   const [updatedPartner, setUpdatedPartner] = useState<Partner>();
+  const [isConfirmDeleteOpen, setConfirmDeleteOpen] = useState(false);
 
   const handleInputKeyPress = async ({ key }: KeyboardEvent) => {
     if (key === 'Enter') {
@@ -38,84 +40,97 @@ function PartnerItem({ partner, setPartners }: Props) {
     cancelEditing();
   }
 
-  async function remove() {
+  const handleRemoval = async () => {
     await removePartner(partner);
 
     setPartners(prevPartners => {
       const { [partner.id]: _, ...newPartners } = prevPartners;
       return newPartners;
     });
-  }
+  };
 
   const cancelEditing = () => {
     setUpdatedPartner(undefined);
   };
 
-  return partner.id === updatedPartner?.id ? (
-    <Box my={2}>
-      <Box mb={2} pr={2} display="flex" justifyContent="space-between">
-        <Box ml={2} width="100%">
-          <TextField
-            fullWidth
-            label="Назва"
-            value={updatedPartner.name}
-            onChange={e =>
-              setUpdatedPartner({
-                ...updatedPartner,
-                name: e.target.value,
-              })
-            }
-            onKeyPress={handleInputKeyPress}
-          />
+  if (partner.id === updatedPartner?.id) {
+    return (
+      <Box my={2}>
+        <Box mb={2} pr={2} display="flex" justifyContent="space-between">
+          <Box ml={2} width="100%">
+            <TextField
+              fullWidth
+              label="Назва"
+              value={updatedPartner.name}
+              onChange={e =>
+                setUpdatedPartner({
+                  ...updatedPartner,
+                  name: e.target.value,
+                })
+              }
+              onKeyPress={handleInputKeyPress}
+            />
+          </Box>
+
+          <Box ml={2} width="100%">
+            <TextField
+              fullWidth
+              label="Посилання"
+              value={updatedPartner.url}
+              onChange={e =>
+                setUpdatedPartner({
+                  ...updatedPartner,
+                  url: e.target.value,
+                })
+              }
+              onKeyPress={handleInputKeyPress}
+            />
+          </Box>
+
+          <IconButton onClick={update}>
+            <DoneIcon />
+          </IconButton>
+
+          <IconButton onClick={cancelEditing}>
+            <HighlightOffIcon />
+          </IconButton>
         </Box>
 
-        <Box ml={2} width="100%">
-          <TextField
-            fullWidth
-            label="Посилання"
-            value={updatedPartner.url}
-            onChange={e =>
-              setUpdatedPartner({
-                ...updatedPartner,
-                url: e.target.value,
-              })
-            }
-            onKeyPress={handleInputKeyPress}
-          />
-        </Box>
-
-        <IconButton onClick={update}>
-          <DoneIcon />
-        </IconButton>
-
-        <IconButton onClick={cancelEditing}>
-          <HighlightOffIcon />
-        </IconButton>
+        <Divider />
       </Box>
+    );
+  }
 
-      <Divider />
-    </Box>
-  ) : (
-    <ListItem divider>
-      <ListItemText
-        primary={partner.name}
-        secondary={
-          <a href={partner.url} target="_blank" rel="noopener noreferrer">
-            {partner.url}
-          </a>
-        }
+  return (
+    <>
+      <ListItem divider>
+        <ListItemText
+          primary={partner.name}
+          secondary={
+            <a href={partner.url} target="_blank" rel="noopener noreferrer">
+              {partner.url}
+            </a>
+          }
+        />
+
+        <ListItemSecondaryAction>
+          <IconButton onClick={() => setUpdatedPartner(partner)}>
+            <EditIcon />
+          </IconButton>
+
+          <IconButton onClick={() => setConfirmDeleteOpen(true)}>
+            <DeleteIcon />
+          </IconButton>
+        </ListItemSecondaryAction>
+      </ListItem>
+
+      <ConfirmDeleteDialog
+        isOpen={isConfirmDeleteOpen}
+        setOpen={setConfirmDeleteOpen}
+        title={partner.name}
+        onConfirm={handleRemoval}
       />
-
-      <ListItemSecondaryAction>
-        <IconButton onClick={() => setUpdatedPartner(partner)}>
-          <EditIcon />
-        </IconButton>
-
-        <IconButton onClick={remove}>
-          <DeleteIcon />
-        </IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
+    </>
   );
 }
 
